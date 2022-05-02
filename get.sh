c74d03af08aa45b0a469d20c017ce6b67a415200
eNV25 <env252525@gmail.com>
2022-05-02 10:47:32 +0530

    This is tetris 7.1

--- get.sh
+++ get.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-curl -sL 'https://cdn.openbsd.org/pub/OpenBSD/7.0/src.tar.gz' |
+curl -sL 'https://cdn.openbsd.org/pub/OpenBSD/7.1/src.tar.gz' |
 	bsdtar -xvf - --strip-components=2 'games/tetris/*.c' 'games/tetris/*.h' 'games/tetris/tetris.6' 'games/tetris/Makefile'
